4da19077cf7b8191fca2cfe22eff2b104b14d495
Iagor Moraes <iagor2014@outlook.com>
2024-07-19 15:29:13 -0300

    fix: allow autofill of input and remove mask input component

--- app/components/PhoneInput.tsx
+++ app/components/PhoneInput.tsx
@@ -1,8 +1,6 @@
 import { ForwardedRef, forwardRef, useState } from 'react';
 import { Box, InputBaseProps, TextField, TextFieldProps } from '@mui/material';
 
-import { InputMask } from './InputMask';
-
 export type PhoneInputProps = {
   label?: string;
   name?: string;
@@ -60,6 +58,27 @@ function PhoneInputComponent(
     }
   };
 
+  const normalizeValue = (value: string) => {
+    // Step 1: Remove all non-digit characters
+    let digits = value.replace(/\D/g, '');
+
+    // Step 2: Ensure it starts with '1' for the country code if not already
+    if (!digits.startsWith('1')) {
+      digits = '1' + digits;
+    }
+
+    // Step 3: Format the string to '+1 (123) 456-7890'
+    // Note: This assumes the country code '1' is always present and correct
+    const match = digits.match(/(1)(\d{3})(\d{3})(\d{4})$/);
+
+    if (match) {
+      return `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}`;
+    } else {
+      // If the format does not match, return the original modified digits with +1
+      return `+1 ${digits.substring(1)}`;
+    }
+  };
+
   const inputProps: TextFieldProps = {
     inputRef: ref,
     autoFocus,
@@ -81,17 +100,15 @@ function PhoneInputComponent(
     <Box width='100%'>
       {/* Use arbitrary input since the text field will contain formatted values to display on UI */}
       <input name={name} value={value} readOnly hidden />
-      <InputMask
-        mask='+1 (999) 999-9999'
-        maskPlaceholder={null}
-        // if the value prop is passed, use it, otherwise use the value from component state
-        // this allows the parent component to control the value of the input field
-        value={valueProp ?? value}
+      <TextField
+        type='tel'
+        autoComplete='tel'
+        {...inputProps}
+        inputProps={{ ...inputProps?.inputProps, maxLength: 17 }}
+        value={normalizeValue(value || '')}
         onChange={handleChange}
         disabled={disabled}
-      >
-        <TextField {...inputProps} />
-      </InputMask>
+      />
     </Box>
   );
 }
